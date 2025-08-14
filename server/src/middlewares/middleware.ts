4befd5200661c1830b8f0b7a5708b49a4ebad3f3
Alexandru Ion <mr_alexandruion@icloud.com>
2025-08-14 14:43:20 +0300

    fix(auth): set user id correctly from JWT

--- server/src/middlewares/middleware.ts
+++ server/src/middlewares/middleware.ts
@@ -18,9 +18,9 @@ export const protect = (
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
-      userId: string;
+      id: string;
     };
-    req.user = { id: decoded.userId };
+    req.user = { id: decoded.id };
     next();
   } catch (err) {
     res.status(401).json({ message: "Token is not valid" });
