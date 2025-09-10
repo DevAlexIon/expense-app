3fcbea86769bed82946337cc751a76d2400f9cab
Alexandru Ion <mr_alexandruion@icloud.com>
2025-09-10 10:40:40 +0300

    feat(api): switch currency conversion to Frankfurter

--- server/src/utils/convercy.ts
+++ server/src/utils/convercy.ts
@@ -6,10 +6,10 @@ export async function convertCurrency(
   if (from === to) return amount;
 
   const res = await fetch(
-    `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}&access_key=${process.env.API_KEY}`
+    `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
   );
 
   const data = await res.json();
 
-  return data.result ?? amount;
+  return data.rates[to] ?? amount;
 }
